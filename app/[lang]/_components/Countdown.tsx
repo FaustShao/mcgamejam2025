efcd7287d326d928a274d4e963a7e189bdc23037
Zhanpeng Shao <zhanpeng.shao1108@gmail.com>
2024-11-19 21:36:06 -0500

    Update timer

--- app/[lang]/_components/Countdown.tsx
+++ app/[lang]/_components/Countdown.tsx
@@ -1,72 +1,97 @@
-'use client'
+"use client";
 
-import React, {useEffect, useState} from "react";
-import {Language} from "@/app/[lang]/_components/constants";
-import {getDictionary} from "@/app/[lang]/_lib/dictionary";
+import React, { useEffect, useState } from "react";
+import { Language } from "@/app/[lang]/_components/constants";
+import { getDictionary } from "@/app/[lang]/_lib/dictionary";
 
-const Countdown = ({ params } : { params: { lang: Language }}) => {
-    const lang = params.lang;
-    const {
-        root: {
-            misc: {
-                day_abbrev,
-                until_the_jam
-            }
-        }
-    } = getDictionary(lang);
-    const formatWithLeadingZero = (value: any) => (value < 10 ? `0${value}` : value);
-    const [timeLeft, setTimeLeft] = useState({
-        months: 0,
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: "0.00"
-    });
-    useEffect(() => {
-        const targetDate = new Date('January 19, 2024 17:00:00 EST');
-        const interval = setInterval(() => {
-            const now = new Date().getTime();
-            const distance = targetDate.getTime() - now;
+const Countdown = ({ params }: { params: { lang: Language } }) => {
+  const lang = params.lang;
+  const {
+    root: {
+      misc: { day_abbrev, until_the_jam },
+    },
+  } = getDictionary(lang);
+  const formatWithLeadingZero = (value: any) =>
+    value < 10 ? `0${value}` : value;
+  const [timeLeft, setTimeLeft] = useState({
+    months: 0,
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: "0.00",
+  });
+  useEffect(() => {
+    const targetDate = new Date("January 10, 2025 17:00:00 EST");
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = targetDate.getTime() - now;
 
-            const months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30.44)); // Average month length
-            const days = Math.floor((distance % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = (Math.floor((distance % (1000 * 60)) / 1000) + (distance % 1000) / 1000).toFixed(0);
+      const months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30.44)); // Average month length
+      const days = Math.floor(
+        (distance % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24)
+      );
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = (
+        Math.floor((distance % (1000 * 60)) / 1000) +
+        (distance % 1000) / 1000
+      ).toFixed(0);
 
-            setTimeLeft({ months, days, hours, minutes, seconds });
-            if (distance < 0) {
-                clearInterval(interval);
-                setTimeLeft({
-                    months: 0,
-                    days: 0,
-                    hours: 0,
-                    minutes: 0,
-                    seconds: "0.00"
-                });
-            }
-        }, 1);
+      setTimeLeft({ months, days, hours, minutes, seconds });
+      if (distance < 0) {
+        clearInterval(interval);
+        setTimeLeft({
+          months: 0,
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: "0.00",
+        });
+      }
+    }, 1);
 
-        return () => clearInterval(interval);
-    }, []);
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-        <>
-            <div className="flex items-center space-x-2 text-lg font-medium w-72">
-                <div><span className="text-2xl pr-2">{timeLeft.months}</span><span>M</span></div>
-                <div><span className="text-2xl pr-2">{formatWithLeadingZero(timeLeft.days)}</span><span>{day_abbrev}</span></div>
-                <div><span className="text-2xl pr-2">{formatWithLeadingZero(timeLeft.hours)}</span><span>H</span></div>
-                <div><span className="text-2xl pr-2">{formatWithLeadingZero(timeLeft.minutes)}</span><span>MIN</span></div>
-                <div className="flex items-baseline">
-                    <span className="text-2xl w-10 text-left">{formatWithLeadingZero(timeLeft.seconds)}</span>
-                    <span>S</span>
-                </div>
-            </div>
-            <div className="flex items-center ml-4 text-md font-medium justify-center pr-6">
-                <span className="whitespace-nowrap">{until_the_jam}</span>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="flex items-center space-x-2 text-lg font-medium w-72">
+        <div>
+          <span className="text-2xl pr-2">{timeLeft.months}</span>
+          <span>M</span>
+        </div>
+        <div>
+          <span className="text-2xl pr-2">
+            {formatWithLeadingZero(timeLeft.days)}
+          </span>
+          <span>{day_abbrev}</span>
+        </div>
+        <div>
+          <span className="text-2xl pr-2">
+            {formatWithLeadingZero(timeLeft.hours)}
+          </span>
+          <span>H</span>
+        </div>
+        <div>
+          <span className="text-2xl pr-2">
+            {formatWithLeadingZero(timeLeft.minutes)}
+          </span>
+          <span>MIN</span>
+        </div>
+        <div className="flex items-baseline">
+          <span className="text-2xl w-10 text-left">
+            {formatWithLeadingZero(timeLeft.seconds)}
+          </span>
+          <span>S</span>
+        </div>
+      </div>
+      <div className="flex items-center ml-4 text-md font-medium justify-center pr-6">
+        <span className="whitespace-nowrap">{until_the_jam}</span>
+      </div>
+    </>
+  );
+};
 
 export default Countdown;
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
-import {FloatingNavCover} from "@/app/[lang]/_components/FloatingNavCover";
-import {Language} from "@/app/[lang]/_components/constants";
+import { FloatingNavCover } from "@/app/[lang]/_components/FloatingNavCover";
+import { Language } from "@/app/[lang]/_components/constants";
 import LanguageSwitchButton from "@/app/[lang]/_components/LanguageSwitchButton";
 
-export default function Home({
-								 params
-							 }: {	params: { lang: Language }
-							 }) {
-	if (params.lang !== 'en-US' && params.lang !== 'fr-FR') {
-		params.lang = 'en-US';
-	}
-	return (
-		<div className="flex flex-col gt-lg:justify-center w-screen h-screen">
-			<Image src={"/Banner_Unbranded_2024.png"} alt={"McGameJam 2024"}
-				   width="0"
-				   height="0"
-				   sizes="100vw"
-				   className="w-full h-auto"/>
+export default function Home({ params }: { params: { lang: Language } }) {
+  const [floatingNavCover, setFloatingNavCover] = useState<JSX.Element | null>(
+    null
+  );
 
-			<FloatingNavCover params={params}/>
-			<LanguageSwitchButton params={params} currentPath="/[lang]"/>
-		</div>
-	);
+  useEffect(() => {
+    async function loadFloatingNavCover() {
+      const component = await FloatingNavCover({ params });
+      setFloatingNavCover(component);
+    }
+    loadFloatingNavCover();
+  }, [params]);
+
+  if (params.lang !== "en-US" && params.lang !== "fr-FR") {
+    params.lang = "en-US";
+  }
+
+  return (
+    <div className="flex flex-col gt-lg:justify-center w-screen h-screen">
+      <Image
+        src={"/Banner_Unbranded_2024.png"}
+        alt={"McGameJam 2024"}
+        width="0"
+        height="0"
+        sizes="100vw"
+        className="w-full h-auto"
+      />
+
+      {floatingNavCover}
+      <LanguageSwitchButton params={params} currentPath="/[lang]" />
+    </div>
+  );
 }
