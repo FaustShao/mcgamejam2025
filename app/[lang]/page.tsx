afd610ced276258023cdb0123a0cd816d9af0fd3
Zhanpeng Shao <zhanpeng.shao1108@gmail.com>
2024-11-19 21:49:13 -0500

    Update McGame Jam 2025 Banner

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import Image from "next/image";
 
-import {FloatingNavCover} from "@/app/[lang]/_components/FloatingNavCover";
-import {Language} from "@/app/[lang]/_components/constants";
+import { FloatingNavCover } from "@/app/[lang]/_components/FloatingNavCover";
+import { Language } from "@/app/[lang]/_components/constants";
 import LanguageSwitchButton from "@/app/[lang]/_components/LanguageSwitchButton";
 
-export default function Home({
-								 params
-							 }: {	params: { lang: Language }
-							 }) {
-	if (params.lang !== 'en-US' && params.lang !== 'fr-FR') {
-		params.lang = 'en-US';
-	}
-	return (
-		<div className="flex flex-col gt-lg:justify-center w-screen h-screen">
-			<Image src={"/Banner_Unbranded_2024.png"} alt={"McGameJam 2024"}
-				   width="0"
-				   height="0"
-				   sizes="100vw"
-				   className="w-full h-auto"/>
+export default function Home({ params }: { params: { lang: Language } }) {
+  if (params.lang !== "en-US" && params.lang !== "fr-FR") {
+    params.lang = "en-US";
+  }
+  return (
+    <div className="flex flex-col gt-lg:justify-center w-screen h-screen">
+      <Image
+        src={"/Banner_Unbranded_2025.png"}
+        alt={"McGameJam 2025"}
+        width="0"
+        height="0"
+        sizes="100vw"
+        className="w-full h-auto"
+      />
 
-			<FloatingNavCover params={params}/>
-			<LanguageSwitchButton params={params} currentPath="/[lang]"/>
-		</div>
-	);
+      <FloatingNavCover params={params} />
+      <LanguageSwitchButton params={params} currentPath="/[lang]" />
+    </div>
+  );
 }
